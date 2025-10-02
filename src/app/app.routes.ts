2fcf61521d8ed23a0171524e0ed9b2c75bd29f3e
Gabriel Castro <gabrielcastro.developer@gmail.com>
2025-10-02 09:33:43 -0300

    roteamento básico

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
+import { PaginaCadastroComponent } from './pages/pagina-cadastro/pagina-cadastro.component';
+import { PaginaConsultaComponent } from './pages/pagina-consulta/pagina-consulta.component';
 
 export const routes: Routes = [
- 
+    { path: '', redirectTo: '/cadastro', pathMatch: 'full' },
+    { path: 'cadastro', component: PaginaCadastroComponent },
+    { path: 'consulta', component: PaginaConsultaComponent }
 ];
